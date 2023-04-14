f5e56b875cf660110e91747601556e67e89a1ebd
MerelyMortal <102021464+MerelyMortal@users.noreply.github.com>
2023-04-13 20:38:19 -0700

    Update TextProcess.py
    First full draft of Text Processing Part A Assignment

--- TextProcess.py
+++ TextProcess.py
@@ -1,6 +1,3 @@
-import re
-import string
-
 class Token:
     def __init__(token, word, frequency):
         token.word = word
@@ -10,6 +7,22 @@ class Token:
 def tokenize(TextFilePath):
     # returns a list of tokens from the file #
     tokens = TextFilePath.split(); 
-    
     # make a new list 'tokens' that will have each word of TextFilePath in it through use of split() #
-    
+    return tokens;
+
+def computeWordFrequencies(tokens):
+    wordFreqMap = {}
+    # initialize a map
+    for word in tokens:
+        if word in tokens:
+            wordFreqMap[word] += 1
+            # if the map contains the word, increment the value that the word is mapped to by one
+        else: 
+            wordFreqMap[word] = 1
+    return wordFreqMap        
+            # if the map doesn't contain the word, assign the value 1 to that word
+
+def print(wordFreqMap):
+    for key, value in wordFreqMap.items():
+        print(key, " : ", value)
+    # for every key in the map, print out the key and value
